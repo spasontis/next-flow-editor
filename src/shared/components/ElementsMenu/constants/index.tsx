3c1e383558b6fa8202e1f41fed73b8f0da903c25
Dmitrii <smxxtdzmail@gmail.com>
2025-11-02 14:14:59 +0700

    feat: add elements menu (#25)
    **Description**
     - Added elements menu in devtools
     - Deleted handles control from dev node

---
+++ src/shared/components/ElementsMenu/constants/index.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import vertical_node from "@/shared/assets/icons/vertical-node.png";
+import horizontal_node from "@/shared/assets/icons/horizontal-node.png";
+
+export const elementsOptions = [
+  {
+    label: <Image src={vertical_node} alt="vertical node" />,
+    type: "v_in_out",
+  },
+  {
+    label: (
+      <>
+        <Image src={horizontal_node} alt="vertical node" />
+      </>
+    ),
+    type: "h_in_out",
+  },
+];
